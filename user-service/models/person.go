a9498e42170f42f71014fdb18f33615c13a338d7
Andrew J. Greving <andrew.greving@gmail.com>
2020-03-16 16:21:32 -0700

    Added Person model Validation and validation test #2

--- user-service/models/person.go
+++ user-service/models/person.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"regexp"
+	"strings"
+)
+
 // Person data structure.
 // Note: Type is essentially a string enum
 type Person struct {
@@ -9,4 +14,43 @@ type Person struct {
 	Type     string `json:"type" bson:"type" example:"Parent"`
 	Password string `json:"password" bson:"password" example:"ABC123"`
 	FamilyID string `json:"familyID" bson:"familyID" example:""`
+}
+
+// Validate input data for Person
+func (p Person) Validate() bool {
+	if len(p.Name) < 1 || len(p.Name) > 128 {
+		return false
+	} else if !p.validateEmail() {
+		return false
+	} else if !p.validatePassword() {
+		return false
+	} else if !p.validateType() {
+		return false
+	}
+
+	return true
+}
+
+func (p Person) validateEmail() bool {
+	emailRegex := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+	if !emailRegex.MatchString(p.Email) {
+		return false
+	}
+	return true
+}
+
+func (p Person) validatePassword() bool {
+	if len(p.Password) < 8 || len(p.Password) > 128 {
+		return false
+	}
+
+	return true
+}
+
+func (p Person) validateType() bool {
+	if strings.Compare(strings.ToLower(p.Type), "parent") != 0 && strings.Compare(strings.ToLower(p.Type), "child") != 0 {
+		return false
+	}
+
+	return true
 }
