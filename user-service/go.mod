2592b14888d09bc63f634bedfb2f626aff96df91
Andrew J. Greving <gigatar@users.noreply.github.com>
2020-03-19 21:19:26 -0700

    Navigation (#53)
    * Added navigation and moved secure area items to navigation. Closes #51
    
    * Linter fixes

--- user-service/go.mod
+++ user-service/go.mod
@@ -4,9 +4,9 @@ go 1.12
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/gigatar/chorelist v0.0.0-20200317205907-98e15b1f0134
+	github.com/gigatar/chorelist v0.0.0-20200319231027-0033034a0cef
 	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.4
 	go.mongodb.org/mongo-driver v1.3.1
-	golang.org/x/crypto v0.0.0-20200311171314-f7b00557c8c4
+	golang.org/x/crypto v0.0.0-20200317142112-1b76d66859c6
 )
---
+++ webview-service/webfiles/chorelist/src/components/Navigation.vue
@@ -0,0 +1,73 @@
+<template>
+  <b-navbar toggleable="lg" type="light" variant="light" fixed="top">
+    <b-navbar-brand href="#">
+      <img src="@/assets/logo.png" alt="Task Tracker" height="28px" />
+    </b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#/dashboard" active>Dashboard</b-nav-item>
+        <b-nav-item href="#">Chores</b-nav-item>
+        <b-nav-item href="#">Family</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown right block>
+          <template slot="button-content">
+            <!-- Add avatar here and no-caret to dropdown -->
+            {{ userName }}
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item @click="logout">Logout</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+<script>
+export default {
+  name: "Navigation",
+  beforeMount() {
+    this.checkLogin();
+  },
+  mounted() {
+    this.tokenValidationInterval();
+  },
+  computed: {
+    userName() {
+      return this.$store.getters.getUserName;
+    }
+  },
+  methods: {
+    /**
+     * Run checkLogin() every 30s to ensure
+     * our token hasn't expired.
+     */
+    tokenValidationInterval() {
+      setInterval(() => {
+        this.$store.dispatch("fetchToken");
+        if (!this.$store.getters.getTokenValid) {
+          this.$store.dispatch("logout");
+        }
+      }, 30000);
+    },
+    /**
+     * Fetch token and check to make sure it's valid,
+     * logout if not.
+     */
+    checkLogin() {
+      this.$store.dispatch("fetchToken");
+      if (!this.$store.getters.getTokenValid) {
+        this.$store.dispatch("logout");
+      }
+    },
+    /**
+     * Logout of the application.
+     */
+    logout() {
+      this.$store.dispatch("logout");
+    }
+  }
+};
+</script>
