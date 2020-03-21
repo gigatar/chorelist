09f121399211c1cf53559edcf10b1a5c7a381a0e
Andrew J. Greving <gigatar@users.noreply.github.com>
2020-03-21 13:25:04 -0700

    Remove unneeded nav tag and.  Closes #67 (#71)

--- webview-service/webfiles/chorelist/src/components/Navigation.vue
+++ webview-service/webfiles/chorelist/src/components/Navigation.vue
@@ -7,15 +7,13 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav>
-            <b-nav-item to="dashboard" exact exact-active-class="active"
-              >Dashboard</b-nav-item
-            >
-            <b-nav-item href="#">Chores</b-nav-item>
-            <b-nav-item to="family" exact exact-active-class="active"
-              >Family</b-nav-item
-            >
-          </b-nav>
+          <b-nav-item to="dashboard" exact exact-active-class="active"
+            >Dashboard</b-nav-item
+          >
+          <b-nav-item href="#">Chores</b-nav-item>
+          <b-nav-item to="family" exact exact-active-class="active"
+            >Family</b-nav-item
+          >
         </b-navbar-nav>
 
         <!-- Right aligned -->
